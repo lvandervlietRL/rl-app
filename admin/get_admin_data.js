2f807b61ad761aee0966172762a9b44d78be2665
Lex van der Vliet <lvandervliet@ggdghor.nl>
2024-11-08 23:11:43 +0100

    fix

--- admin/get_admin_data.js
+++ admin/get_admin_data.js
@@ -1,5 +1,11 @@
 // get_admin_data.js
 
+const wsElement = document.querySelector('.workspace-id')
+const dashElement = document.querySelector('.workspace-id')
+
+let wsCmsId = wsElement.textContent.trim()
+let dashCmsId = dashElement.textContent.trim()
+
 // Declare global variables
 let firstWebhookData = null;
 let secondWebhookData = null;
