5d31c8c762f53f51f7ff1940e17a17416e6e5f01
Lex van der Vliet <lvandervliet@ggdghor.nl>
2024-11-26 21:11:04 +0100

    color picker

--- admin/site_customization/get_customize_data.js
+++ admin/site_customization/get_customize_data.js
@@ -18,6 +18,35 @@
 // customize-snapchat
 // customize-tiktok
 
+// Create a new color picker instance
+// https://iro.js.org/guide.html#getting-started
+var colorPicker = new iro.ColorPicker(".ms-colorpicker", {
+    // color picker options
+    // Option guide: https://iro.js.org/guide.html#color-picker-options
+    width: 180,
+    color: "rgb(255, 0, 0)",
+    borderWidth: 5,
+    borderColor: "#f5f5f5",
+});    
+var values = document.getElementById("values");
+var hexInput = document.getElementById("hexInput");
+var swatch = document.getElementById("colorSwatch");    // https://iro.js.org/guide.html#color-picker-events
+colorPicker.on(["color:init", "color:change"], function(color){
+    // Show the current color in different formats
+    // Using the selected color: https://iro.js.org/guide.html#selected-color-api
+    values.innerHTML = [
+        "hex: " + color.hexString,
+        "rgb: " + color.rgbString,
+        "hsl: " + color.hslString,
+    ].join("<br>");
+    
+    hexInput.value = color.hexString;
+    swatch.style.backgroundColor = color.hexString;
+});    hexInput.addEventListener('change', function() {
+    colorPicker.color.hexString = this.value;
+    swatch.style.backgroundColor = this.value;
+});
+
 // Function to show the edit modal and populate it with site data
 function showEditModal() {
     try {
