0aeb547bb11be682eb8f4bd35c9249e46e30e990
Lex van der Vliet <lvandervliet@ggdghor.nl>
2024-12-22 22:31:33 +0100

    simplified

--- admin/dashboard/post_new_dashboard.js
+++ admin/dashboard/post_new_dashboard.js
@@ -19,8 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const dashboardName = checkElementValue('.new-dashboard-name', 'Error: Dashboard name cannot be empty.');
             if (dashboardName === null) return;
 
-            const dashboardImage = checkElementValueById('imageUrl', 'Error: Dashboard image cannot be empty.');
-            if (dashboardImage === null) return;
+            const dashboardImage = document.getElementById('imageUrl').textContent;
 
             const dashboardSlug = checkElementValue('.new-dashboard-slug', 'Error: Dashboard slug cannot be empty.');
             if (dashboardSlug === null) return;
